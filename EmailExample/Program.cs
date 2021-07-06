0944c5b23793a651750a98631bcff612b16be057
Deepak Mehta (EXT/DIGITAL) <deepak.mehta@petronas.com>
2021-07-06 15:13:23 +0800

    initial commit

---
+++ EmailExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EmailExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmailService.EmailService emailService = new EmailService.EmailService();
+            emailService.SendEmail().Wait();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EmailService/EmailService.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace EmailService
+{
+    public class EmailService
+    {
+
+        private GraphServiceClient GetProvider()
+        {
+            var clientId = "ClientID";
+            var clientSecret = "ClientSecret";
+            var tenantID = "TenantID";
+            List<string> scopes = new List<string>() { "https://graph.microsoft.com/.default" } ;
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+            .Create(clientId)
+            .WithTenantId(tenantID)
+            .WithClientSecret(clientSecret)
+            .Build();
+
+            GraphServiceClient graphServiceClient =
+                new GraphServiceClient(new DelegateAuthenticationProvider(async (requestMessage) => {
+                    // Retrieve an access token for Microsoft Graph (gets a fresh token if needed).
+                    var authResult = await confidentialClientApplication.AcquireTokenForClient(scopes).ExecuteAsync();
+                    // Add the access token in the Authorization header of the API
+                    requestMessage.Headers.Authorization =
+                    new AuthenticationHeaderValue("Bearer", authResult.AccessToken);
+                })
+            );
+            return graphServiceClient;
+        }
+
+        public async Task SendEmail()
+        {
+            var client = GetProvider();
+            var message = new Message
+            {
+                Subject = "Test",
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Html,
+                    Content = "Email Content"
+                },
+                ToRecipients = new List<Recipient>()
+                {
+                    new Recipient
+                    {
+                        EmailAddress = new EmailAddress
+                        {
+                            Address = "deepak.mehta@petronas.com"
+                        }
+                    }
+                },
+                CcRecipients = new List<Recipient>()
+                {
+                    new Recipient
+                    {
+                        EmailAddress = new EmailAddress
+                        {
+                            Address = "deepak.mehta@petronas.com"
+                        }
+                    }
+                }
+            };
+            //await client.Me.SendMail(message, false).Request().PostAsync();
+            await client.Users["a-abhijit@pethlab.com"].SendMail(message, false).Request().PostAsync();
+
+        }
+    }
+}
